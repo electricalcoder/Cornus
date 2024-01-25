4d57a22381bc19f4bbeaae6b66ca1731ab8e2ddd
Jiahao Wu <1521623845@qq.com>
2024-01-25 14:25:35 +0800

    update

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -1,2 +1,2 @@
 export PKG_CONFIG_PATH=/usr/local/grpc/lib/pkgconfig
-export LD_LIBRARY_PATH=${HOME}/Cornus/src/lib:/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/Cornus/src/lib:/usr/local/lib:$LD_LIBRARY_PATH
