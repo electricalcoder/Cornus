79eea96135e533c49b856a3035f9ccb77d9731ed
Jiahao Wu <1521623845@qq.com>
2024-01-26 22:19:22 +0800

    update

--- tools/netperf.sh
+++ tools/netperf.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 
-for i in $(seq 2 9); do
-    for j in $(seq 2 9); do
+echo Measure latency...
+for i in $(seq 2 10); do
+    echo ====================================================
+    for j in $(seq 2 10); do
 	lat=$(ssh 172.17.0.$i ping -c 1 172.17.0.$j | awk 'BEGIN {FS="[=]|[ ]"} NR==2 {print $10}')
-	echo lat c.$i to c.$j: $lat
-    done
-    for j in $(seq 1 1); do
-	lat=$(ssh 172.17.0.$i ping -c 1 172.17.0.$j | awk 'BEGIN {FS="[=]|[ ]"} NR==2 {print $10}')
-	echo lat c.$i to s.$j: $lat
+	echo lat 172.17.0.$i to 172.17.0.$j: $lat ms
     done
 done
+
+echo Measure BW...
+for i in $(seq 2 10); do
+    ssh 172.17.0.$i iperf -s &
+done
+for i in $(seq 2 10); do
+    echo ====================================================
+    for j in $(seq 2 10); do	
+    	bw=$(ssh 172.17.0.$i iperf -c 172.17.0.$j | awk 'BEGIN {FS="[=]|[ ]"} NR==7 {print $7}'
+	echo bw 172.17.0.$i to 172.17.0.$j: $bw Gbps
+    done
+done
+for i in $(seq 2 10); do
+    ssh 172.17.0.$i pkill iperf
+done
+
