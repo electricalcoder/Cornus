8f8f0f9f6f81da0ba32817eb32f52cb0544a6418
Jiahao Wu <1521623845@qq.com>
2024-01-26 22:41:28 +0800

    update

--- config_ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ssh-keygen -o
-for i in $(seq 2 10); do
-ssh-copy-id root@172.17.0.$i
-done
-
--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,7 @@
 
 for i in $(seq 1 8); do
     echo creating compute node $i
-    docker run -d --name cn$i --hostname cn$i cn
+    docker run --privileged -d --name cn$i --hostname cn$i cn
 done
-docker run -d --name sn0 --hostname sn1 sn
+docker run --privileged -d --name sn0 --hostname sn1 sn
+
