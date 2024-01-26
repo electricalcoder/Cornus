ad9e5b66818aa2eef65b46f0b84f8a779f71168b
Jiahao Wu <1521623845@qq.com>
2024-01-27 00:49:13 +0800

    update

--- src/system/main.cpp
+++ src/system/main.cpp
@@ -43,6 +43,26 @@ int main(int argc, char* argv[])
     pthread_t * pthread_rpc = new pthread_t;
     pthread_create(pthread_rpc, nullptr, start_rpc_server, nullptr);
 #endif
+
+{
+    while (1) {
+    SundialRequest request;
+    SundialResponse response;
+    request.set_request_type( SundialRequest::SYS_REQ );
+    // Notify other nodes the completion of the current node.
+    starttime = get_sys_clock();
+    for (uint32_t i = 0; i < g_num_nodes; i ++) {
+      if (i == g_node_id) continue;
+        SemaphoreSync * sem = new SemaphoreSync();
+        sem->incr();
+        request.set_semaphore(reinterpret_cast<uint64_t>(sem));
+        rpc_client->sendRequestAsync(nullptr, i, request, response);
+    }
+    sem->wait();
+    endtime = get_sys_clock() - starttime;
+    cout << endtime / 1000 << " us" << endl;
+   }}
+
     #if LOG_DEVICE == LOG_DVC_REDIS || LOG_DEVICE == LOG_DVC_CUSTOMIZED
         // assume a shared logging but store different node's info to different key
         cout << "[Sundial] creating Redis client" << endl;
