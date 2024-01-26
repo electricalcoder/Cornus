5769227f90e322f29b7d054d1f493c8dbdbbfec2
Jiahao Wu <1521623845@qq.com>
2024-01-26 22:37:54 +0800

    update

--- tools/sn_tc.sh
+++ tools/sn_tc.sh
@@ -7,15 +7,13 @@ SS_LAT=100ms
 SC_LAT=100ms
 DEFAULT_LAT=100ms
 
-for i in {2..9}; do
-    iptables -t mangle -A PREROUTING -s 172.17.0.$i -j MARK --set-mark 1001
-done
-iptables -t mangle -A PREROUTING -s 172.17.0.10 -j MARK --set-mark 1002
-iptables -t mangle -L -v -n
-
 tc qdisc add dev eth0 root handle 1:0 htb default 3
-tc filter add dev eth0 protocol ip parent 1:0 prio 1 handle 1002 fw classid 1:1
-tc filter add dev eth0 protocol ip parent 1:0 prio 1 handle 1001 fw classid 1:2
+for i in {2..9}; do
+    tc filter add dev eth0 parent 1:0 prior 1 protocol ip u32 match ip dst 172.17.0.$i classid 1:2
+done
+for i in {10..10}; do
+    tc filter add dev eth0 parent 1:0 prior 3 protocol ip u32 match ip dst 172.17.0.$i classid 1:1
+done
 tc class add dev eth0 parent 1:0 classid 1:1 htb rate $SS_BW
 tc class add dev eth0 parent 1:0 classid 1:2 htb rate $SC_BW
 tc class add dev eth0 parent 1:0 classid 1:3 htb rate $DEFAULT_BW
