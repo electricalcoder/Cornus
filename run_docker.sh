99b8e45a902dcde849039cdccccb5c32cdb2261a
Jiahao Wu <1521623845@qq.com>
2024-01-26 22:42:48 +0800

    update

--- run_docker.sh
+++ run_docker.sh
@@ -4,5 +4,5 @@ for i in $(seq 1 8); do
     echo creating compute node $i
     docker run --privileged -d --name cn$i --hostname cn$i cn
 done
-docker run --privileged -d --name sn0 --hostname sn1 sn
+docker run --privileged -d --name sn1 --hostname sn1 sn
 
